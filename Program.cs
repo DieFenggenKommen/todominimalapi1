e3fee62f36b41f3fd7c34f2ae81facc8000f2c6e
DieFenggenKommen <109066495+DieFenggenKommen@users.noreply.github.com>
2022-08-29 15:26:31 +0200

    Activated Swagger also in Production

--- Program.cs
+++ Program.cs
@@ -24,14 +24,9 @@ builder.Services.AddScoped<IToDoRepository, ToDoRepository>();
 
 var app = builder.Build();
 
-
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
